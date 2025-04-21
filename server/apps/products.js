338a1bf2c26d1e31534c554acc376e383346ebe8
spxhub1864 <spxhub1864@th.corp.seagroup.com>
2025-04-21 14:59:18 +0700

    create api for product

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,153 @@
 import { Router } from "express";
+import { db } from "../utils/db.js"
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+const collection = db.collection("products");
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+    //1 select collection
+    //2 access req and body
+    const keywords = req.query.keywords;
+    const category = req.query.category;
+    const filter = {};
+    if (keywords) {
+        filter.name = { $regex: keywords, $options: "i" };
+    }
+    if (category) {
+        filter.category = category;
+    }
+    //3 execute
+    try {
+        const result = await collection
+        .find(filter)
+        .toArray();
+        //4 res section
+        if (!result) {
+            return res.status(404).json({
+                "message": "Product not found"
+            })
+        }
+        return res.status(200).json({
+            "data" : result 
+    });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            "message": "500 Internal Server Error"
+        })
+    }
 
-productRouter.post("/", (req, res) => {});
+});
 
-productRouter.put("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+    //1 select collection
+    //2 access req and body
+    const productIdFromClient = req.params.id;
+    const filter = {_id: new ObjectId(productIdFromClient)}
+    //3 execute
+    try {
+        const result = await collection
+        .findOne(filter, {projection: {image: 0}});
+        //4 res section
+        if (!result) {
+            res.status(404).json({
+                "message": "Product not found"
+            })
+        }
+        return res.status(200).json({
+            "data": result
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            "message": "500 Internal Server Error"
+        })
+    }
 
-productRouter.delete("/:id", (req, res) => {});
+});
+
+productRouter.post("/", async (req, res) => {
+    //1 select collection
+    //const collection = db.collection("products");
+    //2 access req and body
+    const newProduct = {...req.body,
+        "created_at": new Date()
+    };
+    //3 execute
+    try {
+        const result = await collection
+        .insertOne(
+            newProduct
+        );
+        //4 res section
+        return res.status(201).json({
+            "message": "Product has been created successfully",
+            "product_id": result.insertedId
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            "message": "500 Internal Server Error"
+        });
+    }
+    
+});
+
+productRouter.put("/:productId", async (req, res) => {
+    //1 select collection
+    //2 access req and body
+    const productIdFromClient = req.params.productId;
+    const updateProduct = {...req.body};
+    const filter = {_id: new ObjectId(productIdFromClient)}
+    //3 execute
+    try {
+        const result = await collection
+        .updateOne(filter,{$set: updateProduct})
+        //4 res section
+        if (result.matchedCount === 0){
+            return res.status(404).json({
+                "message": "Product not found"
+            })
+        }
+        return res.status(200).json({
+            "message": "Product has been updated successfully"
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            "message": "500 Internal Server Error"
+        });
+    }
+
+});
+
+productRouter.delete("/:id", async (req, res) => {
+    //1 select collection
+    //2 access req and body
+    const productIdFromClient = req.params.id;
+    const filter = {_id: new ObjectId(productIdFromClient)};
+    //3 execute
+    try {
+        const result = await collection
+        .deleteOne(filter);
+        //4 res section
+        if (result.deletedCount === 0){
+            return res.status(404).json({
+                "message": "Product not found"
+            })
+        }
+        return res.status(200).json({
+            "message": "Product has been deleted successfully"
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            "message": "500 Internal Server Error"
+        })
+    }
+
+});
 
 export default productRouter;
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,9 @@
 // Set up db connection here
+import { MongoClient } from "mongodb";
+
+const connectionString = "mongodb://127.0.0.1:27017";
+const option = {useUnifiedTopology: true}
+
+export const client = new MongoClient(connectionString, option);
+
+export const db = client.db("practice-mongo");
